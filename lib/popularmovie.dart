4125f6fc150aa01b9f0715e6308c87d35606fd55
mlatifr <mlatifraldi@gmail.com>
2021-04-28 08:36:59 +0700

    mencari movie

--- lib/popularmovie.dart
+++ lib/popularmovie.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-int hlm = 1;
 List<PopMovie> PMs = [];
 
 // class PopMovie ==> menampung object
@@ -27,29 +25,6 @@ class PopMovie {
   }
 }
 
-Widget DaftarPopMovie() {
-  if (PMs.length > 0) {
-    return ListView.builder(
-        itemCount: PMs.length,
-        itemBuilder: (BuildContext ctxt, int index) {
-          return Card(
-            child: Row(
-              children: [
-                Image.network(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6IzKeimIZzmkyQvHw9v9vo5F5isIXyrVDOw&usqp=CAU'),
-                new Text(
-                  PMs[index].id.toString() + ' ' + PMs[index].title,
-                  textAlign: TextAlign.justify,
-                ),
-              ],
-            ),
-          );
-        });
-  } else {
-    return CircularProgressIndicator();
-  }
-}
-
 class PopularMovie extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -60,44 +35,78 @@ class PopularMovie extends StatefulWidget {
 class _PopularMovieState extends State<PopularMovie> {
   String _temp = 'menunggu API';
   Future<String> fetchData() async {
-    final response = await http
-        .get(Uri.http("ubaya.prototipe.net", '/daniel/movielist.php'));
+    final response = await http.post(
+        Uri.http("ubaya.prototipe.net", '/daniel/movielist.php'),
+        body: {'cari': _txtcari});
+
+    // final response = await http
+    //     .get(Uri.http("ubaya.prototipe.net", '/daniel/movielist.php'));
     if (response.statusCode == 200) {
+      // print('responnya : ' + response.body);
       // Future.delayed ==> untuk delay agar muncul gbr loadingnya
-      return Future.delayed(Duration(seconds: 2), () => response.body);
+      return Future.delayed(Duration(seconds: 1), () => response.body);
     } else {
       throw Exception('Failed to read API');
     }
   }
 
-  // bacaData() {
-  //   Future<String> data = fetchData();
-  //   data.then((value) {
-  //     setState(() {
-  //       _temp = value;
-  //     });
-  //   });
-  // }
-  bacaData(int halaman) {
-    Future<String> data = fetchData();
+  String _txtcari = '';
+  Future<String> data;
+  bacaData() {
+    PMs.clear();
+    data = fetchData();
     data.then((value) {
       Map json = jsonDecode(value);
-      // var mov ==>Object
       for (var mov in json['data']) {
         PopMovie pm = PopMovie.fromJson(mov);
         PMs.add(pm);
       }
-      setState(() {
-        _temp = PMs[halaman].title;
-      });
+      setState(() {});
     });
+  }
+
+  Widget DaftarPopMovie() {
+    final ScrollController _scrollController = ScrollController();
+    if (PMs.length > 0) {
+      return Scrollbar(
+        thickness: 20,
+        isAlwaysShown: true,
+        controller: _scrollController,
+        child: ListView.builder(
+            controller: _scrollController,
+            itemCount: PMs.length,
+            itemBuilder: (BuildContext ctxt, int index) {
+              return new Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.movie,
+                        size: 30,
+                      ),
+                      title: Text(PMs[index].title),
+                      subtitle: Text(PMs[index].overview),
+                    ),
+                    // new Text(
+                    //   PMs[index].id.toString() + ' ' + PMs[index].title,
+                    //   textAlign: TextAlign.justify,
+                    // ),
+                  ],
+                ),
+              );
+            }),
+      );
+    } else {
+      return CircularProgressIndicator();
+    }
   }
 
   @override
   void initState() {
     super.initState();
     //bacaData(); ditaruh disini agar saat di buka, data nda dipanggil terus
-    bacaData(1);
+    bacaData();
   }
 
   @override
@@ -105,16 +114,19 @@ class _PopularMovieState extends State<PopularMovie> {
     return Scaffold(
         appBar: AppBar(title: const Text('Popular Movie')),
         body: ListView(children: <Widget>[
-          Container(
-              height: MediaQuery.of(context).size.height - 2,
-              child: Center(child: DaftarPopMovie())),
-          TextButton(
-            onPressed: () {
-              hlm++;
-              bacaData(hlm);
+          TextFormField(
+            decoration: const InputDecoration(
+              icon: Icon(Icons.search),
+              labelText: 'Judul mengandung kata:',
+            ),
+            onFieldSubmitted: (value) {
+              _txtcari = value;
+              bacaData();
             },
-            child: Text(hlm.toString()),
-          )
+          ),
+          Container(
+              height: MediaQuery.of(context).size.height - 100,
+              child: Center(child: DaftarPopMovie())),
         ]));
   }
 }
